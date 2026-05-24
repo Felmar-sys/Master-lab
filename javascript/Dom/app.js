406c4342fc8dc757808b5816311ae5f11b026d2b
FelmArch <ferrlinda.dl@gmail.com>
2026-05-24 10:17:43 +0200

    feat: adicionar h3 e um texto azul de cor azul na Dom

--- javascript/Dom/app.js
+++ javascript/Dom/app.js
@@ -6,3 +6,9 @@ paragrafo.textContent = "Ei, eu sou vermelho!";
 paragrafo.style.color = "red";
 
 container.appendChild(paragrafo);
+
+const subTitulo = document.createElement("h3");
+subTitulo.textContent = "Eu sou um h3 azul!";
+subTitulo.style.color = "blue";
+
+container.appendChild(subTitulo);

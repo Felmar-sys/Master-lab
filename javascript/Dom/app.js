3697219104ca49f25d33b1d414e060c6e59125ad
FelmArch <ferrlinda.dl@gmail.com>
2026-05-24 11:00:21 +0200

    feat: Adiciona div rosa com elementos internos e termina o exercicio DOM

--- javascript/Dom/app.js
+++ javascript/Dom/app.js
@@ -12,3 +12,18 @@ subTitulo.textContent = "Eu sou um h3 azul!";
 subTitulo.style.color = "blue";
 
 container.appendChild(subTitulo);
+
+const caixaRosa = document.createElement("div");
+caixaRosa.style.border = "1px solid black";
+caixaRosa.style.backgroundColor = "pink";
+
+const h1CaixaRosa = document.createElement("h1");
+h1CaixaRosa.textContent = "Eu estou numa div.";
+
+caixaRosa.appendChild(h1CaixaRosa);
+
+const pCaixaRosa = document.createElement("p");
+pCaixaRosa.textContent = "EU TAMBEM!";
+
+caixaRosa.appendChild(pCaixaRosa);
+container.appendChild(caixaRosa);
